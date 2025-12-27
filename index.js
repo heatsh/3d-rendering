34a0e0cb199a06f121f58106e5da5f5d7ada3d18
seahowl <ngsh6693@gmail.com>
2025-12-27 17:55:27 +0800

    feat/animate point movement

--- index.js
+++ index.js
@@ -11,31 +11,37 @@ function assertNonNull(thing) {
 
 /**
  * @typedef {Object} Point2D
- * @property {number} x [-1, 1]
- * @property {number} y [-1, 1]
+ * @property {number} x
+ * @property {number} y
  */
 
 /**
  * @typedef {Object} Point3D
- * @property {number} x [-1, 1]
- * @property {number} y [-1, 1]
- * @property {number} z [-1, 1]
+ * @property {number} x
+ * @property {number} y
+ * @property {number} z
  */
 //#endregion
 
+//#region functions
+function clear() {
+  ctx.fillStyle = borderColor;
+  ctx.fillRect(0, 0, maxWidth, maxHeight);
+}
+
 /**
- * @param {Point2D} point
+ * @param {Point2D} point cartesian coordinates of ([-1, 1], [-1, 1])
+ * @returns canvas coordinates of ([0, maxWidth], [0, maxHeight])
  */
 function translate({ x, y }) {
-  // -1..1 | 0..maxHeight/maxWidth
   return {
-    x: (x + 1) / 2 * maxWidth,
-    y: Math.abs(y - 1) / 2 * maxHeight
+    x: ((x + 1) / 2) * maxWidth,
+    y: (Math.abs(y - 1) / 2) * maxHeight,
   };
 }
 
 /**
- * @param {Point2D} point 
+ * @param {Point2D} point
  */
 function point({ x, y }) {
   const size = 10;
@@ -44,14 +50,14 @@ function point({ x, y }) {
 }
 
 /**
- * @param {Point2D} p1 
- * @param {Point2D} p2 
+ * @param {Point2D} p1
+ * @param {Point2D} p2
  */
 function line(p1, p2) {
   ctx.strokeStyle = pointColor;
   ctx.beginPath();
-  ctx.moveTo(p1.x, p1.y)
-  ctx.lineTo(p2.x, p2.y)
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
   ctx.stroke();
 }
 
@@ -62,25 +68,55 @@ function line(p1, p2) {
 function project({ x, y, z }) {
   return {
     x: x / z,
-    y: y / z
-  }
+    y: y / z,
+  };
 }
 
-//#region main
-const graph = document.createElement('canvas');
-document.body.append(graph);
-const ctx = assertNonNull(graph.getContext('2d'));
+/**
+ * @param {Point3D} p
+ * @param {number} dz
+ */
+function translateZ({ x, y, z }, dz) {
+  return {
+    x, y, z: z + dz
+  }
+}
+//#region
 
+//#region main
 const maxWidth = 800;
 const maxHeight = 800;
 const borderColor = 'black';
 const pointColor = 'green';
 
+const graph = document.createElement('canvas');
+document.body.append(graph);
 graph.width = maxWidth;
 graph.height = maxHeight;
-ctx.strokeStyle = borderColor;
-ctx.strokeRect(0, 0, maxWidth, maxHeight);
+const ctx = assertNonNull(graph.getContext('2d'));
 
-point(translate(project({ x: 0.5, y: 0, z: 1 })));
+const fps = 60;
+let dz = 0;
+/**
+ * @type {Array<Point3D>}
+ */
+const points = [
+  { x: 0.5, y: 0.5, z: 0.5 },
+  { x: -0.5, y: 0.5, z: 0.5 },
+  { x: 0.5, y: -0.5, z: 0.5 },
+  { x: -0.5, y: -0.5, z: 0.5 },
+  { x: 0.5, y: 0.5, z: -0.5 },
+  { x: -0.5, y: 0.5, z: -0.5 },
+  { x: 0.5, y: -0.5, z: -0.5 },
+  { x: -0.5, y: -0.5, z: -0.5 },
+];
+(function renderFrame() {
+  dz += 1 / fps;
 
+  clear();
+  for (const p of points) {
+    point(translate(project(translateZ(p, dz))));
+  }
+  setTimeout(renderFrame, 1000 / fps);
+})();
 //#endregion
