00d39c69eea4378f3f6751c04203e30e8ba60976
seahowl <ngsh6693@gmail.com>
2025-12-27 18:58:06 +0800

    feat/draw lines instead of points

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function assertNonNull(thing) {
 
 //#region functions
 function clear() {
-  ctx.fillStyle = borderColor;
+  ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, maxWidth, maxHeight);
 }
 
@@ -45,7 +45,7 @@ function translate({ x, y }) {
  */
 function point({ x, y }) {
   const size = 10;
-  ctx.fillStyle = pointColor;
+  ctx.fillStyle = foregroundColor;
   ctx.fillRect(x - size / 2, y - size / 2, size, size);
 }
 
@@ -54,7 +54,7 @@ function point({ x, y }) {
  * @param {Point2D} p2
  */
 function line(p1, p2) {
-  ctx.strokeStyle = pointColor;
+  ctx.strokeStyle = foregroundColor;
   ctx.beginPath();
   ctx.moveTo(p1.x, p1.y);
   ctx.lineTo(p2.x, p2.y);
@@ -81,13 +81,27 @@ function translateZ({ x, y, z }, dz) {
     x, y, z: z + dz
   }
 }
+
+/**
+ * @param {Point3D} p
+ * @param {number} theta rotation degree
+ * 
+ * @see https://en.wikipedia.org/wiki/Rotation_matrix
+ */
+function rotateAroundY({ x, y, z }, theta) {
+  return {
+    x: x * Math.cos(theta) - z * Math.sin(theta),
+    y, // rotate around y axis, keep y unchanged
+    z: x * Math.sin(theta) + z * Math.cos(theta),
+  };
+}
 //#region
 
 //#region main
 const maxWidth = 800;
 const maxHeight = 800;
-const borderColor = 'black';
-const pointColor = 'green';
+const backgroundColor = 'black';
+const foregroundColor = 'green';
 
 const graph = document.createElement('canvas');
 document.body.append(graph);
@@ -95,28 +109,53 @@ graph.width = maxWidth;
 graph.height = maxHeight;
 const ctx = assertNonNull(graph.getContext('2d'));
 
-const fps = 60;
-let dz = 0;
 /**
  * @type {Array<Point3D>}
  */
 const points = [
   { x: 0.5, y: 0.5, z: 0.5 },
   { x: -0.5, y: 0.5, z: 0.5 },
-  { x: 0.5, y: -0.5, z: 0.5 },
   { x: -0.5, y: -0.5, z: 0.5 },
+  { x: 0.5, y: -0.5, z: 0.5 },
   { x: 0.5, y: 0.5, z: -0.5 },
   { x: -0.5, y: 0.5, z: -0.5 },
-  { x: 0.5, y: -0.5, z: -0.5 },
   { x: -0.5, y: -0.5, z: -0.5 },
+  { x: 0.5, y: -0.5, z: -0.5 },
 ];
+
+/**
+ * @type {Array<number[]>} indexes of points to connect
+ */
+const vectors = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [0, 4],
+  [1, 5],
+  [2, 6],
+  [3, 7],
+]
+
+const fps = 30;
+let dZ = 2;
+let dTheta = 0;
 (function renderFrame() {
-  dz += 1 / fps;
+  const dt = 1 / fps;
+  dZ += 0;
+  dTheta += dt * (Math.PI);
+
 
   clear();
-  for (const p of points) {
-    point(translate(project(translateZ(p, dz))));
-  }
+  const pointsInFrame = points
+    .map(p => rotateAroundY(p, dTheta))
+    .map(p => translateZ(p, dZ))
+    .map(project)
+    .map(translate);
+
+  vectors.forEach(lines => {
+    for (let i = 0; i < lines.length; i++) {
+      line(pointsInFrame[lines[i]], pointsInFrame[lines[(i + 1) % lines.length]]);
+    }
+  })
   setTimeout(renderFrame, 1000 / fps);
 })();
 //#endregion
